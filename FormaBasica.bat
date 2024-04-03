b7d7c62d2f8e2a130e5af8359c290cc7c367d5c0
raijon01 <nicolas.aedoillanes@gmail.com>
2024-04-03 20:09:26 -0300

    backend-20240403-200926

--- FormaBasica.bat
+++ FormaBasica.bat
@@ -1,2 +1,4 @@
-start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\BackendTrabajo & git init & git add . & git commit -m 'MENSAJE' & git remote add origin 'https://github.com/raijon01/backend_asistencia' & git push origin master" 
+
+for /f %%x in ('powershell Get-Date -Format "yyyyMMdd-HHmmss"') do set "mensaje=backend-%%x"
+start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\BackendTrabajo & git init & git add . & git commit -m %mensaje% & git remote add origin 'https://github.com/raijon01/backend_asistencia' & git push origin master" 
 
