503222c137776bb142be1b3298daaeecce9441ef
raijon01 <nicolas.aedoillanes@gmail.com>
2024-04-03 20:34:09 -0300

    backend-20240403-203408

--- FormaBasica.bat
+++ FormaBasica.bat
@@ -1,4 +1,9 @@
 
-for /f %%x in ('powershell Get-Date -Format "yyyyMMdd-HHmmss"') do set "mensaje=backend-%%x"
-start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\BackendTrabajo & git add . & git commit -m %mensaje% & git push origin master" 
+for /f %%x in ('powershell Get-Date -Format "yyyyMMdd-HHmmss"') do set "mensajeback=backend-%%x"
+for /f %%x in ('powershell Get-Date -Format "yyyyMMdd-HHmmss"') do set "mensajefrontweb=frontweb-%%x"
+for /f %%x in ('powershell Get-Date -Format "yyyyMMdd-HHmmss"') do set "mensajefrontmobile=frontmobile-%%x"
+
+start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\BackendTrabajo & git add . & git commit -m %mensajeback% & git push origin master" 
+start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\FrontWebAsistencia & git add . & git commit -m %mensajefrontweb% & git push origin master" 
+start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\FrontCelular & git add . & git commit -m %mensajefrontmobile% & git push origin master" 
 
