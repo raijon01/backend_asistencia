a7513b70ddd5d53aab619d4c713a5b58ba9421a5
raijon01 <nicolas.aedoillanes@gmail.com>
2024-04-03 20:05:38 -0300

    'MENSAJE'

--- FormaBasica.bat
+++ FormaBasica.bat
@@ -1,6 +1,2 @@
-start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\BackendTrabajo" 
-git init
-git add .
-git commit -m "MENSAJE"
-git remote add origin "https://github.com/raijon01/backend_asistencia"
-git push origin master
+start cmd.exe /k "cd C:\Users\nicolas\Desktop\BACKNODE\BackendTrabajo & git init & git add . & git commit -m 'MENSAJE' & git remote add origin 'https://github.com/raijon01/backend_asistencia' & git push origin master" 
+
